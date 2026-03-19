d22226a33768cd6210bfcbc8982cd8faf5c58f9b
Jahir Hernandez <jahirlozano6@gmail.com>
2026-03-19 17:51:48 -0600

    Navegación de ventanas y corrección de errores

--- app/src/main/java/com/example/psm/first_app/SegundoAvancePSM.kt
+++ app/src/main/java/com/example/psm/first_app/SegundoAvancePSM.kt
@@ -1,8 +1,10 @@
 package com.example.psm.first_app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.psm.R
@@ -12,6 +14,14 @@ class SegundoAvancePSM : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_segundo_avance_psm)
+
+        val btnVerMenu = findViewById<CardView>(R.id.btnVerMenu)
+
+        btnVerMenu.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
