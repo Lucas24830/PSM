7929c3992e827eb277e486e01f968920945b8ad2
Jahir Hernandez <jahirlozano6@gmail.com>
2026-03-26 12:52:17 -0600

    Ventana de mis pedidos

--- app/src/main/java/com/example/psm/first_app/MiPedidoActivity.kt
+++ app/src/main/java/com/example/psm/first_app/MiPedidoActivity.kt
@@ -1,34 +1,17 @@
 package com.example.psm.first_app
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.psm.R
 
-class SegundoAvancePSM : AppCompatActivity() {
+class MiPedidoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_segundo_avance_psm)
-
-        val btnVerMenu = findViewById<CardView>(R.id.btnVerMenu)
-
-        btnVerMenu.setOnClickListener {
-            val intent = Intent(this, MenuActivity::class.java)
-            startActivity(intent)
-        }
-
-        val btnMiPedido = findViewById<CardView>(R.id.btnMiPedido)
-
-        btnMiPedido.setOnClickListener {
-            val intent = Intent(this, MiPedidoActivity::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_mi_pedido)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
